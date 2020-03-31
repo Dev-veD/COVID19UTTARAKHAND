06e4ed364b30285c4101a89def6aba1baafe1b49
Dev-veD <divyansh.code@gmail.com>
2020-03-31 20:47:55 +0530

    first draft

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php include "headn.html";?>
+<?php include "headerD.html";?>
+<?php include "headerM.html";?>
+
+
+<body class="animsition">
+    <div class="page-wrapper">
+       
+        
+<div class='text-center'><h1>Uttarakhand COVID19</h1></div>
+        <!-- PAGE CONTENT-->
+        <div class="page-content--bgf7">
+            <!-- STATISTIC-->
+            <section class="statistic statistic2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-3">
+                            <div class="statistic__item statistic__item--green">
+                                <h2 class="number">2</h2>
+                                <span class="desc">Recovered</span>
+                                <div class="icon">
+                                    <i class="zmdi zmdi-account-o"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="statistic__item statistic__item--orange">
+                                <h2 class="number">7</h2>
+                                <span class="desc">Total Cases</span>
+                                <div class="icon">
+                                    <i class="zmdi zmdi-shopping-cart"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="statistic__item statistic__item--blue">
+                                <h2 class="number">5</h2>
+                                <span class="desc">Active</span>
+                                <div class="icon">
+                                    <i class="zmdi zmdi-calendar-note"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="statistic__item statistic__item--red">
+                                <h2 class="number">0</h2>
+                                <span class="desc">Deceased</span>
+                                <div class="icon">
+                                    <i class="zmdi zmdi-money"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- END STATISTIC-->
+            <?php include "tableM.html";?>
+
+            <?php include "footm.html";?>
